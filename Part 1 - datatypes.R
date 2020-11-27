12b8944786ebfdb55a8b6de20a5702120138fe34
Ankur <ankur_roy@outlook.com>
2020-11-27 18:29:58 +0545

    Part 1

---
+++ Part 1 - datatypes.R
@@ -0,0 +1,18 @@
+logic_type <- TRUE
+print(class(logic_type))
+
+numeric_type <- 12.3
+print(class(numeric_type))
+
+integer_type <- 13L
+print(class(integer_type))
+
+complex_type <- 12+4i
+print(class(complex_type))
+
+character_type <- "Characters are for characterizing"
+print(class(character_type))
+
+raw_type <- charToRaw("Hello")
+print(raw_type)
+print(class(raw_type))
